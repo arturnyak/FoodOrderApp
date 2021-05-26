e3a21f12e8bf813fe1c356c5c7e678ef14eb4ceb
arturnyak <arturnyak98@gmail.com>
2021-05-26 08:39:44 -0400

    eslintcache

--- src/App.js
+++ src/App.js
@@ -29,14 +29,3 @@ const App = () => {
 };
 
 export default App;
-
-// const duplicateCount = text => {    https://github.com/arturnyak/FoodOrderApp.git
-
-// }
-
-// console.log(duplicateCount(""), 0);
-// console.log(duplicateCount("abcde"), 0);
-// console.log(duplicateCount("aabbcde"), 2);
-// console.log(duplicateCount("aabBcde"), 2,"should ignore case");
-// console.log(duplicateCount("Indivisibility"), 1)
-// console.log(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
